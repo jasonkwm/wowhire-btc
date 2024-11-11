2d1a3c44f9874abf9f9c5be0e2a56ef92423de05
Jason <jasonkwm13@gmail.com>
2024-11-11 19:58:22 +0800

    fix: login page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,22 +3,24 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="text-center space-y-6">
-        <div className="flex items-center ">
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      <div className="text-center p-12 shadow-2xl">
+        <div className="flex items-center mb-12">
           <Image src="/wow-doge.jpeg" width={100} height={100} alt="doge with wow word" className="rounded-full mr-8" />
-          <h1 className="text-3xl font-semibold text-gray-800">WOW HIRE!</h1>
+          <h1 className="text-4xl font-semibold text-gray-800">WOW HIRE!</h1>
         </div>
-        <div className="flex md:space-x-4 space-y-4 md:space-y-0">
-          <Link href="/get-hired">
-            <button className="px-8 py-4 bg-blue-600 text-white rounded-lg hover:bg-primary transition duration-300">
-              GET HIRED
-            </button>
+        <div className="flex flex-col gap-4">
+          <Link
+            href="/get-hired"
+            className="px-8 py-4 bg-blue-600 text-white font-semibold text-xl rounded-lg bg-primary transform transition-transform duration-300 hover:scale-110"
+          >
+            GET HIRED
           </Link>
-          <Link href="/hire-someone">
-            <button className="px-8 py-4 bg-green-600 text-white rounded-lg hover:bg-green-700 transition duration-300">
-              HIRE SOMEONE
-            </button>
+          <Link
+            href="/hire-someone"
+            className="px-8 py-4 bg-green-600 text-white font-semibold text-xl rounded-lg bg-secondary transform transition-transform duration-300 hover:scale-110"
+          >
+            HIRE SOMEONE
           </Link>
         </div>
       </div>
