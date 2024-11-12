cb66ef100d7f3fc74597550bedfc04666dd68e19
Zer <livefatezz@gmail.com>
2024-11-12 07:43:21 +0700

    feat: get-hired UI

--- frontend/src/app/get-hired/page.tsx
+++ frontend/src/app/get-hired/page.tsx
@@ -1,17 +1,42 @@
-import JobDetails from "@/components/JobDetails";
+// import JobDetails from "@/components/JobDetails";
+
+// export default function Page() {
+//   return (
+//     <JobDetails
+//       imageUrl="/wowhire.jpg"
+//       jobTitle="Cashier"
+//       company="Starbucks"
+//       location="92/4, Floor 2, Sathorn Thani 2 Building, North Sathorn Road, Si Lom, Bang Rak, Bangkok 10500"
+//       workDay="Wednesday"
+//       workHours="8am-12pm"
+//       requirements={["Making Coffee", "Talking to people", "Art on Latte"]}
+//       salary="500"
+//       description="If coffee is your canvas, and mornings don’t start without that perfect pour, you might be our next Barista Extraordinaire. We’re on the hunt for someone who can transform beans and water into magic and serve it up with just the right blend of charm and speed."
+//     />
+"use client";
+
+import { useState } from "react";
+import Header from "./Header";
+import JobPostings from "./JobPostings";
+import * as Dialog from "@radix-ui/react-dialog";
+import JobModal from "./JobModal";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 
 export default function Page() {
+  const [selectedId, setSelectedId] = useState(0);
+
   return (
-    <JobDetails
-      imageUrl="/wowhire.jpg"
-      jobTitle="Cashier"
-      company="Starbucks"
-      location="92/4, Floor 2, Sathorn Thani 2 Building, North Sathorn Road, Si Lom, Bang Rak, Bangkok 10500"
-      workDay="Wednesday"
-      workHours="8am-12pm"
-      requirements={["Making Coffee", "Talking to people", "Art on Latte"]}
-      salary="500"
-      description="If coffee is your canvas, and mornings don’t start without that perfect pour, you might be our next Barista Extraordinaire. We’re on the hunt for someone who can transform beans and water into magic and serve it up with just the right blend of charm and speed."
-    />
+    <div className="w-[600px] max-w-[95%]">
+      <Header />
+      <JobPostings setSelectedId={setSelectedId} />
+      <Dialog.Root open={selectedId !== 0} onOpenChange={(open) => !open && setSelectedId(0)}>
+        <Dialog.Portal>
+          <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+          <Dialog.Content asChild>
+            <JobModal setSelectedId={setSelectedId} />
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -43,18 +43,18 @@ function HirePortal() {
     <div className="w-screen max-w-[1920px] flex h-screen items-center justify-around">
       <Image src={wowHire} alt="logo" className="rounded-full w-[40%]" />
       <div className="w-full max-w-80 h-full flex flex-col items-start justify-center">
-        <p className="w-full text-center text-7xl font-header text-black/80">WOWHIRE</p>
-        <p className="text-5xl font-normal text-black font-bold">Slave away,</p>
-        <p className="text-5xl font-normal text-black font-bold">Work to Earn!</p>
+        <p className="w-full text-center text-7xl font-header /80">WOWHIRE</p>
+        <p className="text-5xl font-normal  font-bold">Slave away,</p>
+        <p className="text-5xl font-normal  font-bold">Work to Earn!</p>
         <Link
           href="/get-hired"
-          className="w-48 py-4 mt-10 text-center text-black font-semibold text-xl rounded-lg bg-primary transform transition-transform duration-300 hover:scale-110"
+          className="w-48 py-4 mt-10 text-center  font-semibold text-xl rounded-lg bg-primary transform transition-transform duration-300 hover:scale-110"
         >
           GET HIRED
         </Link>
         <Link
           href="/hire-someone"
-          className="w-48 py-4 mt-5 text-center text-black font-semibold text-xl rounded-lg border border-primary transform transition-transform duration-300 hover:scale-110"
+          className="w-48 py-4 mt-5 text-center  font-semibold text-xl rounded-lg border border-primary transform transition-transform duration-300 hover:scale-110"
         >
           HIRE SOMEONE
         </Link>
