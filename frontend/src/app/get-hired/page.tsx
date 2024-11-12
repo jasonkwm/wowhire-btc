b35a12e193629d64e423a200709d5c3c93cedbe0
Jason <jasonkwm13@gmail.com>
2024-11-12 08:20:29 +0800

    feat: job details

--- frontend/src/app/get-hired/page.tsx
+++ frontend/src/app/get-hired/page.tsx
@@ -1,3 +1,17 @@
+import JobDetails from "@/components/JobDetails";
+
 export default function Page() {
-  return <h1>GET HIRE MTFK!</h1>;
+  return (
+    <JobDetails
+      imageUrl="/wowhire.jpg"
+      jobTitle="Cashier"
+      company="Starbucks"
+      location="92/4, Floor 2, Sathorn Thani 2 Building, North Sathorn Road, Si Lom, Bang Rak, Bangkok 10500"
+      workDay="Wednesday"
+      workHours="8am-12pm"
+      requirements={["Making Coffee", "Talking to people", "Art on Latte"]}
+      salary="500"
+      description="If coffee is your canvas, and mornings don’t start without that perfect pour, you might be our next Barista Extraordinaire. We’re on the hunt for someone who can transform beans and water into magic and serve it up with just the right blend of charm and speed."
+    />
+  );
 }
